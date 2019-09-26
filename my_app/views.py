f87b5bf1d7a15be3b7cee96df908af8316c2971e
Daniel Didah <didahdaniel@gmail.com>
2019-09-26 15:01:46 +0300

    Added a view for new_search

--- my_app/views.py
+++ my_app/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 # Are used to return the webpages
 def home(request):
     return render(request, 'base.html')
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    send_to_frontend = {
+        'search': search,
+    }
+    return render(request, 'my_app/new_search.html',send_to_frontend)
